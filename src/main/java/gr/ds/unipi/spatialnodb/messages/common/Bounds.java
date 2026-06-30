7df6e7070bf23da9b3c3d0119ab4b2d122acae7a
Nikos Koutroumanis <nikos_koutroumanis@hotmail.com>
2026-06-30 19:58:01 +0300

    modifications for removing the time dimension

--- src/main/java/gr/ds/unipi/spatialnodb/messages/common/Bounds.java
+++ src/main/java/gr/ds/unipi/spatialnodb/messages/common/Bounds.java
@@ -10,10 +10,8 @@ public class Bounds implements Serializable {
 
     private double minLon = Double.MAX_VALUE;
     private double minLat = Double.MAX_VALUE;
-    private long   minTs  = Long.MAX_VALUE;
     private double maxLon = -Double.MAX_VALUE;
     private double maxLat = -Double.MAX_VALUE;
-    private long   maxTs  = Long.MIN_VALUE;
 
     public void add(List<Tuple3<Double, Double, Long>> stPoints) {
 
@@ -32,17 +30,11 @@ public class Bounds implements Serializable {
             if (Double.compare(minLat, stP._2()) == 1) {
                 minLat = stP._2();
             }
-            if (Long.compare(minTs, stP._3()) == 1) {
-                minTs =  stP._3();
-            }
             if (Double.compare(maxLon, stP._1()) == -1) {
                 maxLon = stP._1();
             }
             if (Double.compare(maxLat, stP._2()) == -1) {
                 maxLat = stP._2();
-            }
-            if (Long.compare(maxTs,  stP._3()) == -1) {
-                maxTs =  stP._3();
             }
         }
     }
@@ -50,19 +42,15 @@ public class Bounds implements Serializable {
     public void add(TrajectorySegment ts) {
         minLon = Math.min(minLon, ts.getMinLongitude());
         minLat = Math.min(minLat, ts.getMinLatitude());
-        minTs  = Math.min(minTs,  ts.getMinTimestamp());
         maxLon = Math.max(maxLon, ts.getMaxLongitude());
         maxLat = Math.max(maxLat, ts.getMaxLatitude());
-        maxTs  = Math.max(maxTs,  ts.getMaxTimestamp());
     }
 
     public void merge(Bounds o) {
         minLon = Math.min(minLon, o.minLon);
         minLat = Math.min(minLat, o.minLat);
-        minTs  = Math.min(minTs,  o.minTs);
         maxLon = Math.max(maxLon, o.maxLon);
         maxLat = Math.max(maxLat, o.maxLat);
-        maxTs  = Math.max(maxTs,  o.maxTs);
     }
 
     public double getMinLongitude() {
@@ -71,17 +59,11 @@ public class Bounds implements Serializable {
     public double getMinLatitude() {
         return minLat;
     }
-    public long getMinTimestamp() {
-        return minTs;
-    }
     public double getMaxLongitude() {
         return maxLon;
     }
     public double getMaxLatitude() {
         return maxLat;
-    }
-    public long getMaxTimestamp() {
-        return maxTs;
     }
 
 }
